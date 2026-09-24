efef42db9c4649bf0b966324a0a649bf9c59bc06
storeplayapp9123-cloud <storeplayapp9123@gmail.com>
2026-09-24 16:53:27 +0530

    Update Project.java

--- app/src/main/java/com/aalamstudio/app/Project.java
+++ app/src/main/java/com/aalamstudio/app/Project.java
@@ -3,15 +3,17 @@ package com.aalamstudio.app;
 public class Project {
     public String name;
     public String packageName;
-    public String type;      // "App" or "Game"
-    public String detail;    // orientation (App) or 2D/3D (Game)
+    public String type;
+    public String detail;
+    public String language;
     public long timestamp;
 
-    public Project(String name, String packageName, String type, String detail, long timestamp) {
+    public Project(String name, String packageName, String type, String detail, String language, long timestamp) {
         this.name = name;
         this.packageName = packageName;
         this.type = type;
         this.detail = detail;
+        this.language = language;
         this.timestamp = timestamp;
     }
 }
