8e7c9377238fb0bceff817dbefc5ee9ad602aa9b
storeplayapp9123-cloud <storeplayapp9123@gmail.com>
2026-09-25 15:37:22 +0530

    Update Project.java

--- app/src/main/java/com/aalamstudio/app/Project.java
+++ app/src/main/java/com/aalamstudio/app/Project.java
@@ -6,14 +6,16 @@ public class Project {
     public String type;
     public String detail;
     public String language;
+    public String platform;
     public long timestamp;
 
-    public Project(String name, String packageName, String type, String detail, String language, long timestamp) {
+    public Project(String name, String packageName, String type, String detail, String language, String platform, long timestamp) {
         this.name = name;
         this.packageName = packageName;
         this.type = type;
         this.detail = detail;
         this.language = language;
+        this.platform = platform;
         this.timestamp = timestamp;
     }
 }
